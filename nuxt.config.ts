d59747ba2b473f8637caaf6bb96f661ae4ffff4c
Bravco <boss.wiw@gmail.com>
2023-11-17 20:59:51 +0100

    Added favicon and basic metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,28 @@ export default defineNuxtConfig({
   css: [
     "@/assets/main.css",
   ],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "sk",
+      },
+      title: "Najlacnejšia autopožičovňa",
+      meta: [
+        {
+          name: "author",
+          content: "Andrej Hamráček",
+        },
+        {
+          name: "title",
+          content: "EasyCar.sk - Najlacnejšia Autopožičovňa",
+        },
+        {
+          name: "description",
+          content: "Najlacnejšia autopožičovňa. Autopožičovňa v mestách Poprad, Kežmarok, Levoča, Spišská Nová Ves. Letisko Poprad, autobusová a vlaková stanica Poprad.",
+        },
+      ],
+    },
+  },
   image: {
     provider: "netlify",
     netlify: process.env.IMAGES_URL,
